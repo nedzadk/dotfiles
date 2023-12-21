7d52536959fb31b3307768f56a53a3e09bab8d84
Nedzad Kulelija <nedzadk@gmail.com>
2023-12-21 20:44:31 +0100

    Add FZF to telescope

--- nvim/lua/ncko/plugins.lua
+++ nvim/lua/ncko/plugins.lua
@@ -16,4 +16,6 @@ require('lazy').setup {
 		'numToStr/Comment.nvim',
 		opts = {}
 	},
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+
 }
