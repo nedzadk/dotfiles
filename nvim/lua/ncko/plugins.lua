7b2d4dce743a5c0394b1483dc476ff99050d1719
Nedzad Kulelija <nedzadk@gmail.com>
2024-07-04 22:25:26 +0200

    Updated neovim lsp

--- nvim/lua/ncko/plugins.lua
+++ nvim/lua/ncko/plugins.lua
@@ -13,5 +13,12 @@ require("lazy").setup({
   },
   {
     'nvim-telescope/telescope.nvim'
-  }
+  },
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
 })
