b7bfa194d66884a118577a207a24734220437f22
Nedzad Kulelija <nedzadk@gmail.com>
2024-07-04 22:16:34 +0200

    Update to neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = " "
 require('ncko/lazy')
 require('ncko/set')
 require('ncko/plugins')
--- nvim/lua/ncko/plugins.lua
+++ nvim/lua/ncko/plugins.lua
@@ -10,5 +10,8 @@ require("lazy").setup({
   },
   {
     "github/copilot.vim"
+  },
+  {
+    'nvim-telescope/telescope.nvim'
   }
 })
