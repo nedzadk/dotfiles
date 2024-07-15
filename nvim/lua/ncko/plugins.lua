ddddc48cb12c439172025a7d2e0c771b107125c2
Nedzad Kulelija <nedzadk@gmail.com>
2024-07-15 19:04:31 +0200

    update

--- nvim/lua/ncko/plugins.lua
+++ nvim/lua/ncko/plugins.lua
@@ -33,5 +33,54 @@ require("lazy").setup({
       'nvim-lua/plenary.nvim', -- required
       'nvim-tree/nvim-web-devicons', -- optional
     }
-  }
+  },
+  -- Animate fancy
+  {
+    'echasnovski/mini.animate'
+  },
+  -- Buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+  -- Git config
+  {
+    "dinhhuy258/git.nvim",
+    event = "BufReadPre",
+    opts = {
+      keymaps = {
+        -- Open blame window
+        blame = "<Leader>gb",
+        -- Open file/folder in git repository
+        browse = "<Leader>go",
+      },
+    },
+  },
+  -- Noice
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
 })
